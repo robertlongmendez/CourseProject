e2d50b91a0a34c413c3f813297da1ac3ce33d13f
robertlongmendez <robertlongmendez@gmail.com>
2021-04-29 03:28:31 -0500

    Course Project setup
    First part of course project set up

---
+++ recipe-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipes[] = [
+    new Recipe('Test recipe, Probably a test, https://cpb-us-e1.wpmucdn.com/blogs.uoregon.edu/dist/c/829/files/2012/10/PSD_Food_illustrations_3190_pancakes_with_butter-1wi1tz5.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
