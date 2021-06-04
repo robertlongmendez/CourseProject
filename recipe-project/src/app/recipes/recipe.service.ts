5870e57c754088140e4fb0464e7eb93242427d74
robertlongmendez <82781708+robertlongmendez@users.noreply.github.com>
2021-06-03 20:10:00 -0500

    simple corrections

--- recipe-project/src/app/recipes/recipe.service.ts
+++ recipe-project/src/app/recipes/recipe.service.ts
@@ -10,27 +10,27 @@ export class RecipeService {
 
   private recipes: Recipe[] = [
     new Recipe('Meat Cakes',
-    'Traditional cakes',
-    'https://cpb-us-e1.wpmucdn.com/blogs.uoregon.edu/dist/c/829/files/2012/10/PSD_Food_illustrations_3190_pancakes_with_butter-1wi1tz5.jpg',
-    [
-      new Ingredient('Meat', 1),
-      new Ingredient('French Fries', 20)
-    ]),
+      'Traditional cakes',
+      'https://cpb-us-e1.wpmucdn.com/blogs.uoregon.edu/dist/c/829/files/2012/10/PSD_Food_illustrations_3190_pancakes_with_butter-1wi1tz5.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
 
     new Recipe('Fish Cakes',
-    'Fun cakes',
-    'https://cpb-us-e1.wpmucdn.com/blogs.uoregon.edu/dist/c/829/files/2012/10/PSD_Food_illustrations_3190_pancakes_with_butter-1wi1tz5.jpg',
-    [
-      new Ingredient('Fun', 200),
-      new Ingredient('More Fun', 300)
-    ])
+      'Fun cakes',
+      'https://cpb-us-e1.wpmucdn.com/blogs.uoregon.edu/dist/c/829/files/2012/10/PSD_Food_illustrations_3190_pancakes_with_butter-1wi1tz5.jpg',
+      [
+        new Ingredient('Fun', 200),
+        new Ingredient('More Fun', 300)
+      ])
 
   ];
 
-  constructor(private slService: ShoppingListService) {}
+  constructor(private slService: ShoppingListService) { }
 
-getRecipes() {
-  return this.recipes.slice();
+  getRecipes() {
+    return this.recipes.slice();
   }
 
   getRecipe(index: number) {
